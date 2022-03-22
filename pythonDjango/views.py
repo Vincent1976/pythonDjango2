ae8d4d356ed798ec513dacb9b1fdbd05181dfbd9
Vincent1976 <jiangxinmin@163.com>
2022-03-22 17:37:04 +0800

    初始化

---
+++ pythonDjango/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(requst):
+    return HttpResponse("Hello world!")
+
+def runoob(request):
+    context={}
+    context['hello'] = 'Hello world!'
+    context['name'] = '菜鸟教程'
+    context['view_list'] = ['教程1', '教程2', '教程3']
+    context['views_str'] = "<a href='https://www.runoob.com/'>点击跳转</a>"
+    context['views_dict'] = {"name":"菜鸟教程", "level": "wonderful", "pages":18}
+    return render(request, 'runoob.html', context)
+
+
+def index(request):
+    return render(request, 'layout.html')
