14d7af6e1425ed95abd99f5132a44d8efbfc4832
Vincent1976 <jiangxinmin@163.com>
2022-03-23 14:17:52 +0800

    登陆机制

--- pythonDjango/urls.py
+++ pythonDjango/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('runoob/', views.runoob),
-    path('index/', views.index)
+    path('index/', views.index),
+    path('login/', views.login)
 ]
